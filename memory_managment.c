3bbbc07f9e4df43086ef3a19cfa9dd34f1451175
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-13 11:24:40 +0100

    hot fix

---
+++ memory_managment.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+/**
+ * @free_all - frees all the malloc'd memory
+ * 
+ * Return: void
+ */
+void free_all(void)
+{
+	if (inventory != NULL)
+	{
+		if (inventory->file != NULL)
+			fclose(inventory->file);
+		
+		if (inventory->line != NULL)
+			free(inventory->line);
+		
+		if (inventory->input != NULL)
+			free(inventory->input);
+		
+		free_stack();
+		free(inventory);
+	}
+}
+/**
+ * free_stack - frees all malloc'd memory in stack
+ * 
+ * Return: void
+ */
+void free_stack(void)
+{
+	stack_t *temp;
+	stack_t *head;
+
+	if(inventory->stack)
+	{
+		head = inventory->stack;
+
+		while(head)
+		{
+			temp = head->next;
+			free(head);
+			head = temp;
+		}
+	}
+}
--- monty.h
+++ monty.h
@@ -92,7 +92,13 @@ void error_handler(char *error_msg);
 void (*monty_cmd_handler(void))(stack_t **stack, unsigned int line_number);
 int areDigits(char *num);
 
+
 int inventory_builder(void);
 int line_parser(char *line);
+ 
+/* ------ MEMORY MANAGMENT ----*/
+
+void free_all(void);
+void free_stack(void);
 
 #endif
