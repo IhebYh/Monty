7d85abc50677b97b315003d7bc9a10b5979b299f
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-01-13 12:11:32 +0100

    testing + code fix

--- opcodes_1.c
+++ opcodes_1.c
@@ -40,6 +40,6 @@ void _pall(stack_t **stack, unsigned int line_number)
 	stack_t *copy = *stack;
 	(void)line_number;
 
-	while (copy->next)
+	for (; copy; copy = copy->next)
 		printf("%d\n", copy->n);
 }
